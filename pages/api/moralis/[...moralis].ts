bbf1db6a4a628d0dce1fba3648ae330fe7d2f498
AcaciaCornigera <84410078+acaciacornigera@users.noreply.github.com>
2023-03-28 13:30:34 +0400

    Update [...moralis].ts

--- pages/api/moralis/[...moralis].ts
+++ pages/api/moralis/[...moralis].ts
@@ -3,7 +3,7 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
   apiKey: process.env.MORALIS_API_KEY || '',
   authentication: {
-    domain: 'https://your-wallet-tracker.vercel.app/',
+    domain: process.env.DOMAIN,
     uri: process.env.NEXTAUTH_URL || '',
     timeout: 120,
   },
